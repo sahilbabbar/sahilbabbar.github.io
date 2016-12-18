1bc87a13e382652f4836280c50309e6203742ed7
Sahil Babbar <sahilbabbar@live.com>
2016-12-18 17:21:05 +0530

    Gulpified Project

--- scripts/directives.js
+++ scripts/directives.js
@@ -5,12 +5,12 @@
         return {
             restrict: 'E',
             templateUrl: 'templates/header.html'
-        }
+        };
     });
     app.directive('footer', function() {
         return {
             restrict: 'E',
             templateUrl: 'templates/footer.html'
-        }
+        };
     });
 })();
