1c0489f8c439dde9d85dbef2560402deccaaed74
Sahil Babbar <sahilbabbar@live.com>
2017-07-25 01:47:20 +0530

    Redirection Page - UP

--- scripts/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(function() {
-    var app = angular.module('meApp', ['ui.router', 'ngMaterial']);
-    // app.config(['$locationProvider', function($locationProvider) {
-    //     $locationProvider.hashPrefix('');
-    // }]);
-
-})();
-
--- scripts/controllers.js
+++
@@ -1,9 +0,0 @@
-// 'use strict';
-// (function() {
-//     var app = angular.module('shlApp');
-//
-//     app.controller('HeaderCtrl', ['$scope', function($scope) {
-//         $scope.currentPage = '/';
-//     }]);
-//
-// })();
